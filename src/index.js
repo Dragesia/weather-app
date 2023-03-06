f1beb1dd39a5a2cdafb9b9970637124042d236b0
Dragesia <brkgzlsy@gmail.com>
2023-03-06 19:15:05 +0300

    change font and add localStorage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const body = document.querySelector("body");
 
 const city = document.querySelector(".city");
+const clock = document.querySelector(".hour");
 const time = document.querySelector(".time");
 const temp = document.querySelector(".temp");
 const img = document.querySelector(".main > img");
@@ -25,11 +26,11 @@ input.onkeydown = (e) => {
 async function getWeather(city) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=16cee363107451176fe52b1753cbc50c&units=metric`);
     const data = await response.json();
-    console.log(data);
     
     if (data.cod == "404") {
         return 0;
     }
+    localStorage.setItem("lastCity", city);
     return data;
 }
 
@@ -38,37 +39,48 @@ async function renderPage(dataPrm) {
     
     if (!data) return;
 
+    let hour = new Date().getHours();
+    let minutes = new Date().getMinutes();
+    let day = new Date().getDate();
+    let month = new Date().getMonth() + 1;
+    let year = new Date().getFullYear();
+
+    let fullHour = hour + ":" + minutes;
+    let fullDate = day + "." + month + "." + year;
+
     city.innerHTML = data.name;
-    time.innerHTML =  new Date().toLocaleString();
+    clock.innerHTML = fullHour;
+    time.innerHTML =  fullDate;
     temp.innerHTML = Math.round(data.main.temp) + "&#8451;";
-    weather.innerHTML = data.weather[0].main;
+    // weather.innerHTML = data.weather[0].main;
     feelslike.innerHTML = Math.round(data.main.feels_like) + "&#8451;";
     wind.innerHTML = data.wind.speed + "km/h";
     humidity.innerHTML = data.main.humidity + "%";
     pressure.innerHTML = data.main.pressure + "hPa";
 
     if (data.weather[0].main == "Clear") {
-        img.src = "media/ico/mainclear.png";
+        // img.src = "media/ico/mainclear.png";
         body.style.backgroundImage = `url(${bgImages[0]})`;
     } else if (data.weather[0].main == "Clouds") {
-        img.src = "media/ico/maincloud.png";
+        // img.src = "media/ico/maincloud.png";
         body.style.backgroundImage = `url(${bgImages[1]})`;
     } else if (data.weather[0].main == "Atmosphere") {
-        img.src = "media/ico/mainatmosphere.png";
+        // img.src = "media/ico/mainatmosphere.png";
         body.style.backgroundImage = `url(${bgImages[2]})`;
     } else if (data.weather[0].main == "Rain") {
-        img.src = "media/ico/mainrain.png";
+        // img.src = "media/ico/mainrain.png";
         body.style.backgroundImage = `url(${bgImages[1]})`;
     } else if (data.weather[0].main == "Drizzle") {
-        img.src = "media/ico/maindrizzle.png";
+        // img.src = "media/ico/maindrizzle.png";
         body.style.backgroundImage = `url(${bgImages[1]})`;
     } else if (data.weather[0].main == "Thunderstorm") {
-        img.src = "media/ico/mainthunderstorm.png";
+        // img.src = "media/ico/mainthunderstorm.png";
         body.style.backgroundImage = `url(${bgImages[1]})`;
     } else if (data.weather[0].main == "Snow") {
-        img.src = "media/ico/mainsnow.png";
+        // img.src = "media/ico/mainsnow.png";
         body.style.backgroundImage = `url(${bgImages[2]})`;
     }
 }
 
 renderPage(getWeather("Manchester"));
+renderPage(getWeather(localStorage.getItem("lastCity")));
